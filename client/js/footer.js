181b3096f830e248c56ffdfe1c7831d7a4f99585
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-03-26 10:59:56 +0530

    register page done

--- client/js/footer.js
+++ client/js/footer.js
@@ -2,7 +2,7 @@ const createFooter = () => {
     let footer = document.querySelector('footer');
 
     footer.innerHTML = `
-    <p class="footer-des">copyright &copy2011-2018 sabka bazaar grocery supplies pvt ltd</p>
+    <p class="footer-des">copyright &copy 2011-2018 sabka bazaar grocery supplies pvt ltd</p>
 
     `
     ;
